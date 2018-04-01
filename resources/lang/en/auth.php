8a2fe853696e18c23cec2b9fef7afbfc4ff36a07
Abdullah Hafizh <abdllhhafizh@gmail.com>
2018-04-01 13:51:26 +0700

    update language

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -4,12 +4,12 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Authentication Language Lines
+    | Baris-baris bahasa untuk autentifikasi
     |--------------------------------------------------------------------------
     |
-    | The following language lines are used during authentication for various
-    | messages that we need to display to the user. You are free to modify
-    | these language lines according to your application's requirements.
+    | Baris bahasa berikut digunakan selama proses autentifikasi untuk beberapa
+    | pesan yang perlu kita tampilkan ke pengguna. Anda bebas untuk memodifikasi
+    | baris bahasa sesuai dengan keperluan aplikasi anda.
     |
     */
 
